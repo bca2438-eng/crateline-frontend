3dad9e1704ee1f7234588fde2a76001337670899
TitusRichard <bca2438@rajagiri.edu>
2026-05-15 15:06:52 +0530

    Add newline at end of api.js and format code

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const API = axios.create({
-baseURL: 'http://localhost:8000',
+  baseURL: 'http://localhost:8000',
+});
+
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
