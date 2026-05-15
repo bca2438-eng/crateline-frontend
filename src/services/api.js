29876efb0f84f8b339511ff5f41d3ac8baa94f14
TitusRichard <bca2438@rajagiri.edu>
2026-05-15 15:38:11 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://absently-voyage-garage.ngrok-free.dev',
 });
 
 API.interceptors.request.use((config) => {
