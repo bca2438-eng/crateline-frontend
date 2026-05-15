c2a2e98539d1d8a5807df5d1ca11614fb0a5ea0a
TitusRichard <bca2438@rajagiri.edu>
2026-05-15 15:11:56 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8000',
-});
+baseURL: 'https://crateline-api-production.up.railway.app',});
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
